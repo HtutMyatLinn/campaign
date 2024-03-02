1dd0b0705bf0c655fc623a8c0904991d511a31af
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2024-03-02 21:52:00 +0630

    ok nar sar campaign

---
+++ components/nav.php
@@ -0,0 +1,26 @@
+<nav class="nav">
+    <i class="uil uil-bars navOpenBtn"></i>
+    <a href="#" class="logo">Social MC</a>
+    <ul class="nav-links">
+        <i class="uil uil-times navCloseBtn"></i>
+        <li><a href="homepage.php">Home</a></li>
+        <li><a href="infoPage.php">Information</a></li>
+        <li><a href="#">Most Popular Social Media Apps</a></li>
+        <li><a href="howParentsCanHelp.php">How Parents Can Help</a></li>
+        <li><a href="livestreaming.php">Livestreaming</a></li>
+        <li>
+            <a href="">Contact</a>
+            <ul>
+                <li><a href="customerLogout.php">Log Out</a></li>
+            </ul>
+
+        </li>
+
+        <li><a href="legislationAndGuidance.php">Legislation and Guidance</a></li>
+    </ul>
+    <i class="uil uil-search search-icon" id="searchIcon"></i>
+    <div class="search-box">
+        <i class="uil uil-search search-icon"></i>
+        <input type="text" placeholder="Search here..." />
+    </div>
+</nav>
---
+++ components/sideBar.php
@@ -0,0 +1,67 @@
+<aside class="sidebar">
+    <div class="logo">
+        <img src="./img/SMC_Logo.jpeg" alt="logo">
+
+        <h2>Social MC</h2>
+
+    </div>
+    <ul class="links">
+        <h4>Main Menu</h4>
+        <li>
+            <span class="material-symbols-outlined">dashboard</span>
+            <a href="adminDashBoard.php">Dashboard</a>
+        </li>
+        <!-- <li>
+            <span class="material-symbols-outlined">show_chart</span>
+            <a href="#">Revenue</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">flag</span>
+            <a href="#">Reports</a>
+        </li> -->
+        <hr>
+        <h4>Advanced</h4>
+        <li>
+            <span class="material-symbols-outlined">campaign</span>
+            <a href="addCampaignType.php">Add Campaign Type</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">media_link</span>
+            <a href="addSocialMedia.php">Add Social Media</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">campaign</span>
+            <a href="addCampaign.php">Add Campaign</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">switch_account</span>
+            <a href="admin.php">Add Profile </a>
+        </li>
+        <!-- <li>
+            <span class="material-symbols-outlined">package_2</span>
+            <a href="./package.php">Add Package</a>
+        </li> -->
+        <hr>
+        <h4>Account</h4>
+        <!-- <li>
+            <span class="material-symbols-outlined">bar_chart</span>
+            <a href="#">Overview</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">mail</span>
+            <a href="#">Message</a>
+        </li>
+        <li>
+            <span class="material-symbols-outlined">settings</span>
+            <a href="#">Settings</a>
+        </li> -->
+        <!-- <li>
+            <span class="material-symbols-outlined">switch_account</span>
+            <a href="#">Add profile </a>
+        </li> -->
+        <li class="logout-link">
+            <span class="material-symbols-outlined">logout</span>
+            <a href="./adminLogout.php">Logout</a>
+        </li>
+    </ul>
+</aside>
